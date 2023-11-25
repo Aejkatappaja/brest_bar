6098fb72aeaff1a65822388db4c211a6331572b8
Aejkatappaja <joverjlj@gmail.com>
2023-11-25 02:59:38 +0100

    add(utils): establishments list

---
+++ utils/establishments-list.ts
@@ -0,0 +1,5 @@
+export const establishments = [
+  { src: '/images/cave.png', text: 'Cave', query: 'cave' },
+  { src: '/images/brewery.png', text: 'Brasserie', query: 'brewery' },
+  { src: '/images/bar.png', text: 'Bar', query: 'bar' },
+];
