f28e72d6fd60f10060d19923560e50a15be0637b
Aejkatappaja <joverjlj@gmail.com>
2023-11-25 03:12:43 +0100

    feat(button): create component

---
+++ components/ui/button.tsx
@@ -0,0 +1,16 @@
+import React, { ReactComponentElement } from 'react';
+
+interface ButtonProps extends React.ComponentPropsWithoutRef<'button'> {}
+
+export const Button: React.FC<ButtonProps> = ({ ...props }) => {
+  return (
+    <div className='flex items-center justify-center pr-4'>
+      <button
+        className='flex items-center gap-2 rounded-lg bg-violet-500 px-4 py-2 text-sm font-bold text-white transition-transform hover:scale-105'
+        onClick={props.onClick}
+      >
+        {props.children}
+      </button>
+    </div>
+  );
+};
