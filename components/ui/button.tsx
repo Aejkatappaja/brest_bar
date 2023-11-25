d7ad79a74aca7554e79f70c4e8993d7695191c7a
Aejkatappaja <joverjlj@gmail.com>
2023-11-25 18:58:41 +0100

    refacto(buttons): ui

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -6,7 +6,7 @@ export const Button: React.FC<ButtonProps> = ({ ...props }) => {
   return (
     <div className='flex items-center justify-center pr-4'>
       <button
-        className='flex items-center gap-2 rounded-lg bg-violet-500 px-4 py-2 text-sm font-bold text-white transition-transform hover:scale-105'
+        className='flex items-center gap-2 rounded-lg bg-violet-500 px-4 py-2 text-sm font-semibold tracking-wide text-white transition-transform hover:scale-105'
         onClick={props.onClick}
       >
         {props.children}
