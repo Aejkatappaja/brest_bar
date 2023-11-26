d67a3d22262541450a24347609b434a2e70fabce
Aejkatappaja <joverjlj@gmail.com>
2023-11-26 02:31:35 +0100

    add(utils): index to export all & establishmentTypeList & generateRatingStars

---
+++ utils/index.ts
@@ -0,0 +1,5 @@
+export * from './establishmentTypeList';
+export * from './establishments-list';
+export * from './generateRatingStars';
+export * from './getEstablishmentStatus';
+export * from './getEstablishmentType';
