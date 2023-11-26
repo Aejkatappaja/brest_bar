04f4f99499545276879b004aa91c025e64901c09
Aejkatappaja <joverjlj@gmail.com>
2023-11-26 02:31:55 +0100

    add(side-nav): import components

--- components/side-nav.tsx
+++ components/side-nav.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import { EstablishmentLinks } from './establishments-links';
+import { FilteredCardsLinks } from './filtered-cards-links';
 import { FilterButton } from './filter-button';
+import EstablishmentsList from './establishments-list';
+import { ExtendsDataButton } from './extends-data-button';
 
-export const SideNav: React.FC = () => {
+export const SideNav = ({ limit }: { limit: number }) => {
   return (
-    <div className='z-5 w-[80%] overflow-y-scroll bg-[#201f23] md:w-[590px]'>
+    <div className='z-5 custom-scrollbar h-full w-[80%] overflow-y-scroll bg-[#201f23] pb-4 scrollbar md:w-[490px]'>
       <div className='border-b-2 border-[#3d3d3d] px-4 py-4 text-start text-4xl font-bold'>
         Trouver le bar qu&apos;il vous faut{' '}
         <span className='bg-gradient-to-r from-[#c213c7] to-[#b063f2] bg-clip-text text-transparent'>
@@ -13,11 +15,15 @@ export const SideNav: React.FC = () => {
       </div>
       <div className='border-b-2 border-[#3d3d3d] px-4 py-4 text-start'>
         <h2 className='text-2xl font-semibold'>Où boire à Brest ?</h2>
-        <EstablishmentLinks />
+        <FilteredCardsLinks />
       </div>
       <div className='flex items-center justify-between px-4 py-4 text-start'>
         <h2 className='text-2xl font-semibold'>Explorer</h2>
         <FilterButton />
+      </div>
+      <EstablishmentsList limit={limit} />
+      <div className='flex items-center justify-center'>
+        <ExtendsDataButton />
       </div>
     </div>
   );
