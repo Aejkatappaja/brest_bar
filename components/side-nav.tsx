1e381640b37635bba5207e92ea1023d815bc6ae3
Aejkatappaja <joverjlj@gmail.com>
2023-11-26 23:27:11 +0100

    add(SideNav): import components and remove limit handled by searchparams logic

--- components/side-nav.tsx
+++ components/side-nav.tsx
@@ -3,10 +3,11 @@ import { FilteredCardsLinks } from './filtered-cards-links';
 import { FilterButton } from './filter-button';
 import EstablishmentsList from './establishments-list';
 import { ExtendsDataButton } from './extends-data-button';
+import { FilterOptions } from './filter-options';
 
-export const SideNav = ({ limit }: { limit: number }) => {
+export const SideNav = () => {
   return (
-    <div className='z-5 custom-scrollbar h-full w-[80%] overflow-y-scroll bg-[#201f23] pb-4 scrollbar md:w-[490px]'>
+    <div className='z-5 custom-scrollbar max-h-full w-[80%] overflow-y-scroll bg-[#201f23] pb-4 scrollbar-thin md:w-[505px] 2xl:w-[848px]'>
       <div className='border-b-2 border-[#3d3d3d] px-4 py-4 text-start text-4xl font-bold'>
         Trouver le bar qu&apos;il vous faut{' '}
         <span className='bg-gradient-to-r from-[#c213c7] to-[#b063f2] bg-clip-text text-transparent'>
@@ -21,7 +22,8 @@ export const SideNav = ({ limit }: { limit: number }) => {
         <h2 className='text-2xl font-semibold'>Explorer</h2>
         <FilterButton />
       </div>
-      <EstablishmentsList limit={limit} />
+      <FilterOptions />
+      <EstablishmentsList />
       <div className='flex items-center justify-center'>
         <ExtendsDataButton />
       </div>
