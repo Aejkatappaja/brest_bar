ba655da271d62064c199300e9b333c2a8b722622
Aejkatappaja <joverjlj@gmail.com>
2023-11-27 15:25:51 +0100

    add(utils): icons file

---
+++ utils/icons.ts
@@ -0,0 +1,27 @@
+import {
+  FaStar,
+  FaStarHalfStroke,
+  FaMagnifyingGlass,
+  FaArrowRight,
+} from 'react-icons/fa6';
+import { GiCrossMark, GiPathDistance } from 'react-icons/gi';
+import { BiSupport } from 'react-icons/bi';
+import { ImPlus } from 'react-icons/im';
+import { IoFilterOutline, IoEyeOutline } from 'react-icons/io5';
+import { FaArrowLeft } from 'react-icons/fa';
+import { GrToast } from 'react-icons/gr';
+
+export {
+  FaStar,
+  FaArrowLeft,
+  FaMagnifyingGlass,
+  FaStarHalfStroke,
+  GiCrossMark,
+  BiSupport,
+  ImPlus,
+  IoEyeOutline,
+  IoFilterOutline,
+  GrToast,
+  GiPathDistance,
+  FaArrowRight,
+};
