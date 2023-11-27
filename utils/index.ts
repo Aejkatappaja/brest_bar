5447a2dea8bb0392758d3f6d95bd28b4c8da97a6
Aejkatappaja <joverjlj@gmail.com>
2023-11-27 15:26:10 +0100

    add(utils): index exports

--- utils/index.ts
+++ utils/index.ts
@@ -3,3 +3,6 @@ export * from './establishments-list';
 export * from './generateRatingStars';
 export * from './getEstablishmentStatus';
 export * from './getEstablishmentType';
+export * from './haversine';
+export * from './icons';
+export * from './search-filter-list';
