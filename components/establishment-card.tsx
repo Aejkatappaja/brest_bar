f144a424a9fc00c0ba2d1a9c1879a817d18516bb
Aejkatappaja <joverjlj@gmail.com>
2023-11-27 11:37:15 +0100

    refacto(establishment-card): rework ui and import components

--- components/establishment-card.tsx
+++ components/establishment-card.tsx
@@ -1,50 +1,46 @@
 import { IEstablishment } from '@/lib/getEstablishmentsList';
-import { IoEyeOutline } from 'react-icons/io5';
+import { GiPathDistance } from 'react-icons/gi';
 import React from 'react';
-import {
-  generateRatingStars,
-  getEstablishmentStatus,
-  getEstablishmentType,
-} from '@/utils';
+import { generateRatingStars, getEstablishmentType } from '@/utils';
+import { ZoomOnMapButton } from './buttons';
 
 interface EstablishmentCardProps {
   item: IEstablishment;
+  distance: number | null;
 }
 
 export const EstablishmentCard: React.FC<EstablishmentCardProps> = ({
   item,
+  distance,
 }) => {
-  const {
-    name,
-    rating,
-    address,
-    user_ratings_total,
-    status,
-    opening_hours,
-    international_phone_number,
-  } = item;
+  const { name, rating, address, user_ratings_total, status, location } = item;
+  const { coordinates } = location;
 
   const stars = generateRatingStars(rating);
   const establishmentType = getEstablishmentType(name);
-  const establishmentStatus = getEstablishmentStatus(status);
+  // const establishmentStatus = getEstablishmentStatus(status);
+
   return (
-    <div className='flex h-full w-full items-center justify-between overflow-hidden rounded-2xl bg-[#2c2c2c] px-6 py-4'>
-      <div className='space-y-1 '>
-        <p className='font-bold text-purple-500'>{establishmentType}</p>
-        <h1 className='text-2xl font-bold'>{name}</h1>
-        <h3 className=' w-[7rem] truncate text-purple-500 md:w-[17rem]'>
-          {address}
-        </h3>
-        <p className='flex items-center gap-2'>
-          <span className='flex'> {stars}</span> {user_ratings_total} avis{' '}
-        </p>
-
-        <p className='text-green-600'>{establishmentStatus}</p>
+    <div className='flex h-full w-full items-center justify-between overflow-hidden rounded-2xl bg-[#2c2c2c] px-6 py-4 shadow-md shadow-black'>
+      <div className='space-y-5 '>
+        <div className='space-y-1'>
+          <p className='font-bold text-purple-500'>{establishmentType}</p>
+          <h1 className='text-2xl font-bold'>{name}</h1>
+          <h2 className=' w-[7rem] truncate text-purple-500 md:w-[17rem]'>
+            {address}
+          </h2>
+          <p className='flex items-center gap-2'>
+            <span className='flex'> {stars}</span> {user_ratings_total}{' '}
+            {user_ratings_total > 0 ? 'avis' : 'aucun avis'}{' '}
+          </p>
+        </div>
+        {distance && (
+          <div className='font-sm flex w-36 items-center justify-center gap-2 rounded-2xl bg-blue-600/20 py-[0.20rem] text-center text-blue-300/80 shadow-md shadow-black'>
+            <GiPathDistance /> {distance} KM
+          </div>
+        )}
       </div>
-
-      <button className=' mr-1 rounded-full border-purple-500 bg-purple-500/30 px-2 py-2'>
-        <IoEyeOutline className='block text-center text-2xl text-purple-500' />
-      </button>
+      <ZoomOnMapButton coordinates={coordinates} />
     </div>
   );
 };
