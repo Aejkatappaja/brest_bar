d5a10886df68fd651c384908ca54ba140f38c3d8
Aejkatappaja <joverjlj@gmail.com>
2023-11-27 11:33:54 +0100

    feat(map and geoloc user):  render

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,7 +1,40 @@
+'use client';
+
+import MapBox from '@/components/map';
+import { useFetch } from '@/hooks/useFetch';
+import { useMapZoomStore } from '@/hooks/useMapZoom';
+import { useUserLocationStore } from '@/hooks/useUserLocationStore';
+import { IEstablishment } from '@/lib/getEstablishmentsList';
+import mapboxgl from 'mapbox-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
+import React from 'react';
+
 export default function Home() {
+  const { setUserLocation, userLocation } = useUserLocationStore();
+
+  React.useEffect(() => {
+    if ('geolocation' in navigator) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const newLocation: [number, number] = [
+            position.coords.latitude,
+            position.coords.longitude,
+          ];
+
+          setUserLocation(newLocation);
+        },
+        (error) => {
+          console.error('Error getting user location:', error.message);
+        }
+      );
+    } else {
+      console.error('Geolocation is not supported by this browser.');
+    }
+  }, [setUserLocation]);
+
   return (
-    <div className=' flex h-full w-full items-center justify-center bg-white text-black'>
-      MAP
-    </div>
+    <>
+      <MapBox />
+    </>
   );
 }
